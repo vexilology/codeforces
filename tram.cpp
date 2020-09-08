8d235258af2b0b69dfdcdb550de72dd65d8c8c89
vexilology <andreyisback@yandex.ru>
2020-09-08 19:39:23 +0400

    tram

---
+++ tram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  int w;
+  int a, b, sum=0, k=0;
+  std::cin >> w;
+
+  for (int i=0; i<w; ++i) {
+    std::cin >> a >> b;
+    sum = (a + k) - b;
+    if (sum < 0)
+      k = 0;
+    else
+      k = sum;
+  }
+
+  std::cout << sum << std::endl;
+  return 0;
+}
